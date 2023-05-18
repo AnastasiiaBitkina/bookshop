46f0c59ffe53694cc025ebc547b544367c0b2cf1
anastasia <anastasia.ethno@gmail.com>
2023-05-18 10:17:12 +0200

    feat: added header

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+let bookList = [];
 
-// Создаем заголовок h1
-var heading = document.createElement("h1");
-heading.innerText = "Bookshop";
-heading.classList.add("title");
+
+fetch('books.json') //path to the file with json data
+        .then(response => {
+            return response.json();
+        })
+        .then(books => {
+            for (let i = 0; i < books.length; i+=1) {
+                console.log (books[i]);
+                const text = document.createElement("p");
+                text.innerText = books[i].title;
+                container.appendChild(text);
+            }
+            console.log(books);
+        });
+
+
 
 // Создаем список ul
-var list = document.createElement("ul");
+/*var list = document.createElement("ul");
 
 // Добавляем элементы li в список
 var item1 = document.createElement("li");
@@ -26,11 +39,27 @@ list.appendChild(item4);
 
 var item5 = document.createElement("li");
 item5.innerText = "Книга 5";
-list.appendChild(item5);
+list.appendChild(item5);*/
 
 // Добавляем заголовок и список в контейнер
-var container = document.getElementById("container");
-container.appendChild(heading);
-container.appendChild(list);
+const container = document.getElementById("container");
+/*container.appendChild(heading)
+container.appendChild(list);*/
 
 
+// Создаем header
+const header = document.createElement("div");
+header.id = "header";
+container.appendChild(header);
+
+// Создаем заглавную картинку 
+const headerPic = document.createElement("img");
+headerPic.id = "header-pic";
+headerPic.src = "headerPic.jpg";
+header.appendChild(headerPic);
+
+// Создаем заголовок h1
+const heading = document.createElement("h1");
+heading.innerText = "Welcome to amazing bookshop!";
+heading.id = 'heading';
+header.appendChild(heading);
